7dbdd3a38068435b2e1f956d4ba9e1b8ceeb06ac
yutarofire <yutaro.ono.418@gmail.com>
2020-08-17 08:22:59 +0900

    Get a single number for input, and output it just as it is

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "os"
+)
 
 func main() {
+  args := os.Args;
+  if len(args) < 2 {
+    panic("Args required")
+  }
+
+  var input string = args[1]
+
   fmt.Println(".intel_syntax noprefix")
   fmt.Println(".globl main")
   fmt.Println("")
   fmt.Println("main:")
-  fmt.Println("  mov rax, 42")
+  fmt.Println("  mov rax, " + input)
   fmt.Println("  ret")
 }
--- test.sh
+++ test.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 go build -o occ main.go
-./occ > tmp.s
+
+input=0
+./occ $input > tmp.s
 gcc --static -o tmp tmp.s
 ./tmp
 output="$?"
-echo $output
+echo "${input} => ${output}"
+
+input=42
+./occ $input > tmp.s
+gcc --static -o tmp tmp.s
+./tmp
+output="$?"
+echo "${input} => ${output}"
