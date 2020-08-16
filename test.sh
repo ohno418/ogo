0833dab07f4cfd8539f612a46420f2710902364c
yutarofire <yutaro.ono.418@gmail.com>
2020-08-17 07:52:27 +0900

    Improve test code

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-rm tmp.s
-go run main.go >> tmp.s
-gcc --static -o main tmp.s
-./main
-echo $?
+go build -o occ main.go
+./occ 0 > tmp.s
+gcc --static -o tmp tmp.s
+./tmp
+output="$?"
+echo $output
